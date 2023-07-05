6cc9dda94f3b9ec4d45bbfa8b7a592547deb6497
Dmitry <zik-element@mail.ru>
2023-07-05 20:48:52 +0300

    fix2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/tasck-manager/'
+  //publicPath: '/tasck-manager/'
+  publicPath: process.env.NODE_ENV === 'tasck-manager'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
 })
